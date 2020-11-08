2165e6d76b43115f03ccb872df8088fbd7f072e5
afshin <afshin.kh.x76@gmail.com>
2020-11-08 18:01:55 +0000

    add post view for individual views an categories

--- Posts/urls.py
+++ Posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import index, post, blog
 
+
 urlpatterns = [
     path('', index),
     path('blog/', blog),
-    path('posts/', post),
+    path('posts/<int:id>/', post, name='post'),
 ]
--- Posts/views.py
+++ Posts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.db.models import Count
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def index(request):
     queryset = Post.objects.all()[:3]
@@ -9,8 +11,37 @@ def index(request):
     return render(request, "index.html", context)
 
 def blog(request):
-    return render(request, "blog.html")
+    categories = Post.objects.values('categories__title').annotate(Count('categories__title'))
+    queryset = Post.objects.all()
+    
+    paginator = Paginator(queryset, 1)
+    page_number = request.GET.get('page')
 
-def post(request):
-    return render(request, "post.html")
+    try:
+        page_obj = paginator.page(page_number)
+    except PageNotAnInteger:
+        page_obj = paginator.page(paginator.page_range[0])
+    except EmptyPage:
+        page_obj = paginator.page(paginator.page_range[0])
+    
+    latest_posts = Post.objects.order_by('-created_on')
+    context = {
+        'page_numbers': paginator.page_range,
+        'categories': categories,
+        'latest_posts': latest_posts,
+        'queryset': page_obj
+    }
+    return render(request, "blog.html", context)
+
+def post(request, id):
+    latest_posts = Post.objects.order_by('-created_on')
+    categories = Post.objects.values('categories__title').annotate(Count('categories__title'))
+    post = get_object_or_404(Post, id=id)
+    context = {
+        'post': post,
+        'categories': categories,
+        'latest_posts': latest_posts,
+
+    }
+    return render(request, "post.html", context)
 
